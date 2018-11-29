99d44b63a2911c5d90832b54eb6eeb78ee31fde7
Prosilentgamer <45443918+Prosilentgamer@users.noreply.github.com>
2018-11-28 21:52:49 -0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import chalk
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(pass_context=True)
+async def hello(ctx):
+    await bot.say("hello")
+  {
+{);
+// THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
